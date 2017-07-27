b1a30f983f9426f3dddce36f0e5a5d89e2b3a98a
Gustavo <j1254019@mvrht.net>
2017-07-27 14:10:33 -0300

    command parser e exit command

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from cliview import StartPage
 import os
+import sys
 
 os.system("clear")
 s = StartPage()
 print(s.build_header())
-print("b [NOME DA BOARD] para acessar.\n")
+print("b [NOME DA BOARD] para acessar, ou exit para sair.\n")
 
 while True:
-    board = input(">>")
+	board = input(">>")
+	command = board.split()
+	
+	#verifica se está chamando uma board
+	if (len(command) > 1 and command[0] == "b"):
+		#TODO: aqui deve chamar uma classe que baixa a página da board e exibe na tela
+		print("TODO: chamar a board " + command[1])
+	
+	#quero sair
+	elif (board == "exit"):
+		sys.exit()
+	
+	#tras a insulina
+	else:
+		print("Comando não reconhecido, tente mais duro.")
